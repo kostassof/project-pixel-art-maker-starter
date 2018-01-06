b0c863b3f75d8224c9f84b930a7b202d7bb945b9
Kostass <kostassof88@gmail.com>
2018-01-06 23:03:48 +0100

    Made it work.

--- designs.js
+++ designs.js
@@ -2,9 +2,26 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-
 function makeGrid() {
+  $('#pixel_canvas').empty();
+  var rows = $('#input_height').val();
+  var columns = $('#input_width').val();
 
-// Your code goes here!
-
+  for (var i = 0; i < rows; i++) {
+    $("#pixel_canvas").append("<tr class=" + i + "></tr>");
+    for (var j = 0; j < columns; j++) {
+      $("."+ i +"").append("<td></td>")
+    };
+  };
 }
+
+$('#button').on("click", function() {
+  event.preventDefault();
+  makeGrid();
+});
+
+$('#pixel_canvas').on('click', 'td', function() {
+  var color = $('input[type="color"]').val();
+  $(this).css("background-color", color);
+});
+
